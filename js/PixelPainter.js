252a1e00b370e915acc4d3fb628f2390cc026811
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-19 16:01:03 -1000

    add 'br' to create a grid.  put for loop into a function.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,46 +1,55 @@
-var currentColor = "blue";
 
 
-function oneButton() {
+////////=========setting the color picker===========/////////
+var color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet' ];
+
+
+var currentColor = color;
+
+
+function oneButton(grid) {
 //one button
 var white_but = document.createElement('button');
 white_but.style.background = currentColor;
 white_but.style.width = '25px';
 white_but.style.height = '25px';
 white_but.appendChild(document.createTextNode(""));
-document.body.appendChild(white_but);
+grid.appendChild(white_but);
 }
 
+function grid(rows, col) {
+  var twoDimArr = [];
+  // var rows = 4;
+  // var col = 4;
+  var numbers = 0;
+  var pixelPainter = document.getElementById('pixelPainter');
+  for(var i = 0; i < rows; i++){
+    var row = [];
+    for(var j = 0; j < col; j++){
+      row.push(numbers++);
+      oneButton(pixelPainter);
 
-var twoDimArr = [];
-var rows = 4;
-var col = 4;
-var numbers = 0;
-
-for(var i = 0; i < rows; i++){
-  var row = [];
-  for(var j = 0; j < col; j++){
-    row.push(numbers++);
-    oneButton();
+    }
+    pixelPainter.appendChild(document.createElement('br'));
+    twoDimArr.push(row);
   }
-  twoDimArr.push(row);
-
-  //oneButton();
-
-// console.log(twoDimArr[i]);
-
-}
 //console.log(twoDimArr[3][3]);
+}
+grid(20,20);
 
 
-var header = document.createElement('h1');
+// var header = document.createElement('h1');
 // document.getElementById('header').innerHTML = "Hello World";
 
-var headerText = document.createTextNode(twoDimArr[3][2]);
-header.appendChild(headerText);
-document.body.appendChild(header);
+// var headerText = document.createTextNode(twoDimArr[3][2]);
+// header.appendChild(headerText);
+// document.body.appendChild(header);
 
-////========select color===========/////
+////========erase button===========/////
+var erase = 'white';
 
+function erase() {
+
+}
 
 
