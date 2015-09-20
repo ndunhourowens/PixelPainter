974278437c125431769686680f2a9d53b1e93719
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-19 17:27:16 -1000

    1. create color picker grid.  2. set colors in the grid  3.  create onclick eventhandler to select color from picker 4.  applied the color picked to the canvas

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,26 +1,23 @@
+var colorSelected = 'white';
 
 
-////////=========setting the color picker===========/////////
-var color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet' ];
-
-
-var currentColor = color;
 
 
 function oneButton(grid) {
 //one button
-var white_but = document.createElement('button');
-white_but.style.background = currentColor;
-white_but.style.width = '25px';
-white_but.style.height = '25px';
-white_but.appendChild(document.createTextNode(""));
-grid.appendChild(white_but);
+  var white_but = document.createElement('button');
+  white_but.style.background = 'white';
+  white_but.style.width = '25px';
+  white_but.style.height = '25px';
+  white_but.appendChild(document.createTextNode(""));
+  grid.appendChild(white_but);
+  white_but.addEventListener('click', function() {
+    this.style.background = colorSelected;
+  });
 }
 
 function grid(rows, col) {
   var twoDimArr = [];
-  // var rows = 4;
-  // var col = 4;
   var numbers = 0;
   var pixelPainter = document.getElementById('pixelPainter');
   for(var i = 0; i < rows; i++){
@@ -28,6 +25,7 @@ function grid(rows, col) {
     for(var j = 0; j < col; j++){
       row.push(numbers++);
       oneButton(pixelPainter);
+
 
     }
     pixelPainter.appendChild(document.createElement('br'));
@@ -52,4 +50,43 @@ function erase() {
 
 }
 
+////////=========setting the color picker===========/////////
 
+///=======color picker grid==========////
+function colorPickerButton(grid, setColorsInPicker) {
+//one button
+  var color_but = document.createElement('button');
+  color_but.style.background = setColorsInPicker;
+  // color_but.style.background = 'black';
+  color_but.style.width = '25px';
+  color_but.style.height = '25px';
+  color_but.appendChild(document.createTextNode(""));
+  grid.appendChild(color_but);
+  color_but.addEventListener('click', function() {
+    colorSelected = setColorsInPicker;
+  });
+}
+function colorPickerGrid() {
+  var color = [['red', 'orange'], ['yellow', 'green'], ['blue', 'purple'] ];
+
+  var twoDimArr = [];
+  var rows = 3;
+  var col = 2;
+  var numbers = 0;
+  var colorPicker = document.getElementById('colorPicker');// connecting html and js
+  for(var i = 0; i < rows; i++){
+    var row = [];
+    for(var j = 0; j < col; j++){
+      row.push(numbers++);
+      var setColorsInPicker = color[i][j];
+      colorPickerButton(colorPicker, setColorsInPicker);
+      console.log(color[i][j]);
+
+    }
+    colorPicker.appendChild(document.createElement('br'));
+    twoDimArr.push(row);
+  }
+}
+colorPickerGrid();
+
+//////==========save the color clicked into memory==========////
